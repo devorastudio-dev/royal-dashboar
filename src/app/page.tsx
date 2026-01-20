267e40d9dbe1e1eead594435f8dc1045ba035927
Devora Studio <devorastudio.dev@gmail.com>
2026-01-20 16:44:28 -0300

    ajuste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-export default function Home() {
-  return null;
-}
+import { redirect } from "next/navigation";
 
+export default function Home() {
+  redirect("/login"); // troque para "/(dashboard)" ou outra rota se preferir
+}
