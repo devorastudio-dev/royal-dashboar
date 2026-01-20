68f9a9411b039f7a64d839bdf451f5beb5840a2b
Devora Studio <devorastudio.dev@gmail.com>
2026-01-20 15:53:09 -0300

    optimization

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function proxy(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const authCookie = request.cookies.get("auth");
   const isLoginPage = request.nextUrl.pathname === "/login";
 
